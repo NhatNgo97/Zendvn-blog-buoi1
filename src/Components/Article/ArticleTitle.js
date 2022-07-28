bcfdac4d08e34603eab2ee9ab40bf3aac6f40ba2
NhatNgo97 <nhatngo9799@gmail.com>
2022-07-28 16:45:22 +1000

    search by keyword

--- src/Components/Article/ArticleTitle.js
+++ src/Components/Article/ArticleTitle.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import parse from "html-react-parser";
 
 export default function ArtilceTitle({ title, slugLink }) {
   return (
     <h2 className="article-item__title">
-      <Link to={slugLink}>{title}</Link>
+      <Link to={slugLink}>{parse(title)}</Link>
     </h2>
   );
 }
